45927a89f528479e737ff76448d5561466f64951
the-piro <thezake0077@gmail.com>
2025-11-18 21:22:45 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from xtra import snd, Btn  
 
 WELCOME = "Hello! I'm Echo Payments Bot."
-IMG = "https://i.ibb.co/xK56gh8W/photo-2025-11-14-13-10-23-7572567765697953804.jpg"
+IMG = "https://i.ibb.co/HfJzg0yW/photo-2025-11-18-15-51-30-7574093630139269152.jpg"
 EFCT = 5104841245755180586
 
 
