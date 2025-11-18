301feb203303eb4158c9bca4d00319616c9c4b9b
the-piro <thezake0077@gmail.com>
2025-11-18 21:31:00 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -1,23 +1,21 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from xtra import snd, Btn  
+from xtra import Btn
 
 WELCOME = "Hello! I'm Echo Payments Bot."
 IMG = "https://i.ibb.co/HfJzg0yW/photo-2025-11-18-15-51-30-7574093630139269152.jpg"
 EFCT = 5104841245755180586
 
-
 @Client.on_message(filters.command("start"))
 async def start_cmd(client, message):
+
     btn = Btn()
     btn.url_button("Repo", "https://github.com/XalFH/Echo-Payments-Bot")
     buttons = btn.build_menu()
 
-    await snd(
-        message,
-        WELCOME,
-        buttons=buttons,
-        photo=IMG,
+    await message.reply_photo(
+        IMG,
+        caption=WELCOME,
+        reply_markup=buttons,
         message_effect_id=EFCT,
         has_spoiler=True
     )
